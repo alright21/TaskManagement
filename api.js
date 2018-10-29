e276e1e428049fc1c1928f3317645a5b08887ec1
Alberto Giust <33547055+ainter21@users.noreply.github.com>
2018-10-29 15:31:15 +0100

    Add some dummy code to api

--- api.js
+++ api.js
@@ -1,1 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+ 
+var courses_offered = [{id: 21, name: 'HCI'},{id: 28, name:'sweng'}];
+
+app.get('/', (req, res) => res.send('Hello World!'));
+ 
+app.get('/courses', (req, res) => {
+   res.json(courses_offered)
+});
+ 
+app.listen(PORT, () => console.log('Example app listening on port'+ PORT)):
 
