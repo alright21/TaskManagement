9beb4b9f4c8ab2c3e6358c0ca3aa4df824ef6fcc
Alberto <ainter21.6@gmail.com>
2018-11-25 18:21:32 +0100

    Add dummy test

--- api.js
+++ api.js
@@ -4,11 +4,13 @@ const PORT = process.env.PORT || 3000
  
 var courses_offered = [{id: 21, name: 'HCI'},{id: 28, name:'sweng'}];
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.status(200).send('Hello World!'));
  
 app.get('/courses', (req, res) => {
-   res.json(courses_offered)
+   res.status.json(courses_offered)
 });
  
-app.listen(PORT, () => console.log('Example app listening on port'+ PORT));
+var server = app.listen(PORT, () => console.log('Example app listening on port'+ PORT));
+
+module.exports = server;
 
---
+++ api.test.js
@@ -0,0 +1,23 @@
+var request = require('supertest');
+
+describe('loading express', function () {
+    var server;
+    beforeEach(function () {
+      server = require('./api');
+    });
+    afterEach(function () {
+      server.close();
+    });
+    it('responds to /', (code)=> {
+    request(server)
+      .get('/')
+      .expect(200, code);
+    });
+    it('404 everything else', function testPath(done) {
+      request(server)
+        .get('/foo/bar')
+        .expect(404, done);
+    });
+
+    it('')
+  });
