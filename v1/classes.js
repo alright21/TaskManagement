d790e6428a9a86f429e7d0f8569159374b0ca2e2
asiasalva <root@DESKTOP-BJQ7PME.localdomain>
2018-11-27 14:31:21 +0100

    testing POST of class

--- v1/classes.js
+++ v1/classes.js
@@ -2,6 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const classes = express.Router();
 
-classes.get('/', (req, res) => res.status(200).send('Hello World!'));
+//classes.get('/', (req, res) => res.status(200).send('Hello World!'));
+
+const postedClasses = [];
+
+classes.post('/', function(req, res) {
+	const newClass = req.body;
+	newClass.id = 1;
+	postedClasses.push(newClass);
+	res.status(201);
+	res.json(newClass).send();
+});
 
 module.exports = classes;
