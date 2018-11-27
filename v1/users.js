02869a380253e3414b000a32120bccdee162aa51
Michael <agne.michele@gmail.com>
2018-11-27 17:22:33 +0100

    Aggiunta interazione col Db e GET con casi di test

--- v1/users.js
+++ v1/users.js
@@ -1,18 +1,96 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pg = require('pg');
 const users = express.Router();
-const uuid = require('uuid-v4');
 
-const postedUsers = [];
+const pool = new pg.Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'taskmanagement',
+	password: 'abc123',
+	port: '5432'
+});
 
-// users.get('/', (req, res) => res.status(200).send('Hello World!'));
+users.use(bodyParser.json());
+users.use(bodyParser.urlencoded({ extended: true }));
 
-users.post('/', function (req, res) {
-   const newUser = req.body;
-   newUser.id = uuid();
-   postedUsers.push(newUser);
-   res.status(201);
-   res.json(newUser).send();
-})
+users.post('/', async (req, res) => {
+	const toInsert = req.body;
+	
+	//Check if user already in
+	let check = await getUserByEmail(toInsert.email);
+	if(check){
+		let resultnegJSON = JSON.parse(JSON.stringify({}));
+		res.status(400).send(resultnegJSON);
+	}else{ //User not in db
+		let result = await postUser(toInsert);
+		if(result){
+			let resultJson = JSON.parse(JSON.stringify(result));
+			res.status(201).send(resultJson);
+		}
+		else{
+			let resultnegJSON = JSON.parse(JSON.stringify({}));
+			res.status(400).send(resultnegJSON);
+		}
+	}
+});
 
+users.get('/:userID',async (req, res) => {
+	const userID = req.params.userID;
+	let result = await getUserById(userID);
+
+	if(result){
+		let resultJSON = JSON.parse(JSON.stringify(result));
+		res.status(200).send(resultJSON);
+	}else{
+		let resultnegJSON = JSON.parse(JSON.stringify({}));
+		res.status(400).send(resultnegJSON);
+	}
+});
+
+//FUNCTIONS INTERFACING WITH THE DB
+async function getUserById(id){
+	if(!id){
+		return null;
+	}else{
+		var queryText = 'SELECT * FROM "user" WHERE id=$1';
+		var queryParams = [id];
+		var result = await pool.query(queryText, queryParams);
+		if(result.rowCount != 0){
+			return result.rows[0];
+		}else{
+			return null;
+		}
+	}
+}
+
+async function postUser(newUser){
+	if(!newUser.name || !newUser.surname || !newUser.email || !newUser.password)
+		return null;
+	else{
+		let queryText = 'INSERT INTO "user" ("name","surname","email","password") VALUES ($1,$2,$3,$4) RETURNING *';
+		let queryParams = [newUser.name, newUser.surname, newUser.email, newUser.password];
+		let result = await pool.query(queryText, queryParams);
+		if(result){
+			return result.rows[0];
+	  	}else{
+			return null;
+		}
+	}
+}
+
+async function getUserByEmail(email){
+	if(!email){
+		return null;
+	}else{
+		var queryText = 'SELECT * FROM "user" WHERE email=$1';
+		var queryParams = [email];
+		var result = await pool.query(queryText, queryParams);
+		if(result.rowCount != 0){
+			return result.rows[0];
+		}else{
+			return null;
+		}
+	}
+}
 module.exports = users;
