fce8b9a6b09b32b108badb7f2ad7b304d7cc511f
asiasalva <root@DESKTOP-BJQ7PME.localdomain>
2018-11-27 17:05:51 +0100

    Modifiche utili per la parte di connessione al database

--- test/classes.test.js
+++ test/classes.test.js
@@ -1,16 +1,18 @@
 const fetch = require('node-fetch');
 const root = 'http://localhost:3000';
+//const insertClassInToDatabase = require('../v1/classes').classes;
 
 var server;
 
 //Example of what a Class should contain
-const exampleClass = {
+const exampleValidClass = {
 	//'id': 5,
 	'name': 'class1',
 	'prof': 'prof1',
 	'assistants': ['assistant1', 'assistant2'], //NB: array's element are type "user"
 	'students': ['student1', 'student2', 'student3'] //same type of assistants
 };
+
 
 //Functions executed before (and after) doing test cases, to open and close
 //the server:
@@ -38,13 +40,14 @@ const postClass = function(newClass){
 //Test cases:
 
 // 1) Testing if the post was successful, looking if the status is "201"
-test('Post class response', () => {
-	return postClass(exampleClass)
+test('Post class response, case of a valid new Class', () => {
+	return postClass(exampleValidClass)
 		.then(postResponse => {expect(postResponse.status).toBe(201)});
 });
 
+//2) Testing wheter the response body is correct.
 test('Post class response body', () => {
-	return postClass(exampleClass)
+	return postClass(exampleValidClass)
 		.then(postResponse => {return postResponse.json()})
 		.then(postResponseJson => {
 			//Object Schema
@@ -68,3 +71,21 @@ test('Post class response body', () => {
 			});
 		});
 });
+
+//3) Testing for insertClassInToDatabase
+
+//PER POTER FUNZIONARE HO BISOGNO DELLA FUNZIONE DI GET CLASS
+
+/*test('to verify if a valid class is added correctly to the db', () => {
+	return insertClassInToDatabase(exampleValidClass)
+		.then(created => {
+			return getClassById(created.id)
+				.then(res => {
+					exampleValidClass.id = res.id;
+					expect(res.name).toBe(exampleValidClass.name);
+				})
+		})
+});*/
+
+//4) DA INSERIRE DOPO IL GET
+// Testare se è valido, allora mi ritorna la classe.
