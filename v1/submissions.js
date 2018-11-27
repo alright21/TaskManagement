6414b7de36748444dc0bbd37e8ba81c457d61522
Alberto <ainter21.6@gmail.com>
2018-11-27 21:53:13 +0100

    Add tests and implementations of put submission

--- v1/submissions.js
+++ v1/submissions.js
@@ -17,14 +17,14 @@ submissions.get('/', (req, res) => res.status(200).send('Hello World!'));
 
 submissions.post('/',async (req, res) =>{
 
-    console.log('post');
-    console.log(req.body);
+    // console.log('post');
+    // console.log(req.body);
     var result = await insertSubmissionIntoDatabase(req.body);
     // var submission = req.body;
     
-    console.log(result);
+    // console.log(result);
     if(result){
-        console.log('entro if');
+        // console.log('entro if');
         var resultJson = JSON.parse(JSON.stringify(result));
         res.status(201).send(resultJson);
     }
@@ -34,6 +34,26 @@ submissions.post('/',async (req, res) =>{
     
     
 });
+
+submissions.put('/:id', async (req, res)=>{
+
+    const id = req.params.id;
+    const toModify = req.body;
+    if(!id){
+        res.status(400).end();
+    }
+
+
+    var result = await updateSubmissionInDatabase(id, toModify);
+
+    if(result){
+        var resultJson = JSON.parse(JSON.stringify(result));
+        await console.log(resultJson + '\n\n\n\n');
+        res.status(201).send(resultJson);
+    }else{
+        res.status(409).end();
+    }
+})
 
 
 async function insertSubmissionIntoDatabase (submission){
@@ -45,17 +65,17 @@ async function insertSubmissionIntoDatabase (submission){
     // var isUser = null;
     // var isTask = null;
     // var isExam = null;
-    console.log("isUser: " + isUser + ", isTask: " + isTask + ", isExam: " + isExam);
+    // console.log("isUser: " + isUser + ", isTask: " + isTask + ", isExam: " + isExam);
     
     if(!isUser || !isExam || !isTask){
-        console.log("Null values");
+        // console.log("Null values");
         return null;
     }else{
 
         var queryText = 'INSERT INTO "submission" ("user","task","exam", "answer") VALUES($1,$2,$3,$4) RETURNING *';
         var queryParams = [submission.user, submission.task, submission.exam, submission.answer]; 
         
-        console.log(submission);
+        // console.log(submission);
         var result = await pool.query(queryText, queryParams);
         if(result){
             return result.rows[0];
@@ -78,9 +98,9 @@ async function getSubmissionById(id){
         var queryParams = [id];
         var result = await pool.query(queryText, queryParams);
         // console.log("result rows: " + Object.getOwnPropertyNames(result.rows[0]).length);
-        for (const i of result.rows) console.log(i)
+        // for (const i of result.rows) console.log(i)
         if(result.rowCount != 0){
-            console.log("result rows: " + result.rows[0]);
+            // console.log("result rows: " + result.rows[0]);
             return result.rows[0];
         }else{
             return null;
@@ -99,7 +119,7 @@ async function getUserById(id){
         var queryParams = [id];
         var result = await pool.query(queryText, queryParams);
         // console.log("result rows: " + Object.getOwnPropertyNames(result.rows[0]).length);
-        for (const i of result.rows) console.log(i)
+        // for (const i of result.rows) console.log(i)
         if(result.rowCount != 0){
             return result.rows[0];
         }else{
@@ -120,7 +140,7 @@ async function getTaskById(id){
         var queryParams = [id];
         var result = await pool.query(queryText, queryParams);
         // console.log("result rows: " + Object.getOwnPropertyNames(result.rows[0]).length);
-        for (const i of result.rows) console.log(i)
+        // for (const i of result.rows) console.log(i)
         if(result.rowCount != 0){
             return result.rows[0];
         }else{
@@ -140,7 +160,7 @@ async function getExamById(id){
         var queryParams = [id];
         var result = await pool.query(queryText, queryParams);
         // console.log("result rows: " +Object.getOwnPropertyNames(result.rows[0]).length);
-        for (const i of result.rows) console.log(i)
+        // for (const i of result.rows) console.log(i)
         if(result.rowCount != 0){
             return result.rows[0];
         }else{
@@ -152,8 +172,50 @@ async function getExamById(id){
 
 
 
+
+
+async function updateSubmissionInDatabase(id, toModify){
+
+    if(!id){
+        return null;
+    }else{
+        var isSubmission = await getSubmissionById(id);
+
+        if(!isSubmission){
+            return null;
+        }else{
+            if(isSubmission.user != toModify.user){
+                return null;
+            }
+            else if(isSubmission.task != toModify.task){
+                return null;
+            }
+            else if(isSubmission.exam != toModify.exam){
+                return null;
+            }else{
+                console.log(id);
+                var queryText = 'UPDATE "submission" SET "answer"=$1,"final_mark"=$2 WHERE "id"=$3 RETURNING *';
+                var queryParams = [toModify.answer,toModify.final_mark, id];
+
+                var result = await pool.query(queryText,queryParams);
+
+                //console.log(result);
+                if(result.rowCount != 0){
+                    return result.rows[0];
+                }else{
+                    return null;
+                }
+
+            }
+        }
+    }
+}
+
+
+
 module.exports = {
     submissions: submissions,
     insertSubmissionIntoDatabase: insertSubmissionIntoDatabase,
-    getSubmissionById: getSubmissionById
+    getSubmissionById: getSubmissionById,
+    updateSubmissionInDatabase: updateSubmissionInDatabase
 }
