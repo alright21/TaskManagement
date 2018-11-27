9f5e18503512ec4ac3b37df2ee5588db213440b1
Alberto <ainter21.6@gmail.com>
2018-11-27 15:07:24 +0100

    Add tests for the creation of a submission

--- v1/submissions.js
+++ v1/submissions.js
@@ -15,47 +15,145 @@ submissions.use(bodyParser.json());
 submissions.get('/', (req, res) => res.status(200).send('Hello World!'));
 
 
-submissions.post('/',(req, res) =>{
+submissions.post('/',async (req, res) =>{
 
     console.log('post');
     console.log(req.body);
-    // var result = await insertSubmissionIntoDatabse(req.body);
+    var result = await insertSubmissionIntoDatabase(req.body);
     // var submission = req.body;
-    var queryText = 'INSERT INTO "submission" ("user","task","exam", "answer") VALUES($1,$2,$3,$4) RETURNING *';
-    var queryParams = [req.body.user, req.body.task, req.body.exam, req.body.answer]; 
     
-
-        pool.query(queryText, queryParams, (err, result) =>{
-
-            if(err){
-                console.log(err.stack);
-                // return null;
-            }else{
-                var resultJson = JSON.parse(JSON.stringify(result.rows[0]));
-                res.status(201).send(resultJson);
-            }
-            
-        });
-    // console.log(result)
-    // if(result){
-    //     console.log('entro if');
-    //     var resultJson = JSON.parse(JSON.stringify(result));
-    //     res.status(201).send(resultJson);
-    // }
-    // else{
-    //     res.status(400).end();
-    // }
+    console.log(result);
+    if(result){
+        console.log('entro if');
+        var resultJson = JSON.parse(JSON.stringify(result));
+        res.status(201).send(resultJson);
+    }
+    else{
+        res.status(400).end();
+    }
     
     
 });
 
 
-function insertSubmissionIntoDatabse(submission){
+async function insertSubmissionIntoDatabase (submission){
 
+    //Qui ci andrà la logica per controllare che user, task e exam esistano per evitare problemi di database
+    var isUser = await getUserById(submission.user);
+    var isTask = await getTaskById(submission.task);
+    var isExam = await getExamById(submission.exam);
+    // var isUser = null;
+    // var isTask = null;
+    // var isExam = null;
+    console.log("isUser: " + isUser + ", isTask: " + isTask + ", isExam: " + isExam);
+    
+    if(!isUser || !isExam || !isTask){
+        console.log("Null values");
+        return null;
+    }else{
+
+        var queryText = 'INSERT INTO "submission" ("user","task","exam", "answer") VALUES($1,$2,$3,$4) RETURNING *';
+        var queryParams = [submission.user, submission.task, submission.exam, submission.answer]; 
+        
+        console.log(submission);
+        var result = await pool.query(queryText, queryParams);
+        if(result){
+            return result.rows[0];
+        }else{
+            return null;
+        }
+    }
     
     
-
-
-
+    
 }
-module.exports = submissions;
+
+async function getSubmissionById(id){
+
+    if(!id){
+        return null;
+    }else{
+
+        var queryText = 'SELECT * FROM "submission" WHERE id=$1';
+        var queryParams = [id];
+        var result = await pool.query(queryText, queryParams);
+        // console.log("result rows: " + Object.getOwnPropertyNames(result.rows[0]).length);
+        for (const i of result.rows) console.log(i)
+        if(result.rowCount != 0){
+            console.log("result rows: " + result.rows[0]);
+            return result.rows[0];
+        }else{
+            return null;
+        }
+
+    }
+}
+
+async function getUserById(id){
+
+    if(!id){
+        return null;
+    }else{
+
+        var queryText = 'SELECT * FROM "user" WHERE id=$1';
+        var queryParams = [id];
+        var result = await pool.query(queryText, queryParams);
+        // console.log("result rows: " + Object.getOwnPropertyNames(result.rows[0]).length);
+        for (const i of result.rows) console.log(i)
+        if(result.rowCount != 0){
+            return result.rows[0];
+        }else{
+            return null;
+        }
+
+    }
+}
+
+
+async function getTaskById(id){
+
+    if(!id){
+        return null;
+    }else{
+
+        var queryText = 'SELECT * FROM "task" WHERE id=$1';
+        var queryParams = [id];
+        var result = await pool.query(queryText, queryParams);
+        // console.log("result rows: " + Object.getOwnPropertyNames(result.rows[0]).length);
+        for (const i of result.rows) console.log(i)
+        if(result.rowCount != 0){
+            return result.rows[0];
+        }else{
+            return null;
+        }
+
+    }
+}
+
+async function getExamById(id){
+
+    if(!id){
+        return null;
+    }else{
+
+        var queryText = 'SELECT * FROM "exam" WHERE id=$1';
+        var queryParams = [id];
+        var result = await pool.query(queryText, queryParams);
+        // console.log("result rows: " +Object.getOwnPropertyNames(result.rows[0]).length);
+        for (const i of result.rows) console.log(i)
+        if(result.rowCount != 0){
+            return result.rows[0];
+        }else{
+            return null;
+        }
+
+    }
+}
+
+
+
+module.exports = {
+    submissions: submissions,
+    insertSubmissionIntoDatabase: insertSubmissionIntoDatabase,
+    getSubmissionById: getSubmissionById
+}
