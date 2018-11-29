25db73ebf6558e433e23da5457c5e2044e3cd370
asiasalva <root@DESKTOP-BJQ7PME.localdomain>
2018-11-29 16:12:28 +0100

    Test cases for GET classes {id}

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const users = require('./v1/users');
 const exams = require('./v1/exams');
 const tasks = require('./v1/tasks');
 const submissions = require('./v1/submissions').submissions;
-const classes = require('./v1/classes');
+const classes = require('./v1/classes').classes;
 const reviews = require('./v1/reviews');
 
 app.use(bodyParser.json());
