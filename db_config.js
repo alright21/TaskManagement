e81432dc1d2fd3b8be086bfeba08a636daa0cb6f
Alberto <alberto.giust@studenti.unitn.it>
2018-12-06 15:26:06 +0100

    set db for test and for deploy

--- db_config.js
+++ db_config.js
@@ -1,9 +1,23 @@
-const config = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'travis_ci_test',
-    password: 'root',
-    port: '5432'
+
+const config = {};
+if(process.env.TEST){
+    config = {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'travis_ci_test',
+        password: 'root',
+        port: '5432'
+    }
+}else{
+    config = {
+        user: process.env.USER,
+        password: process.env.PASS,
+        database: process.env.DATABASE,
+        port: process.env.PORT_DB,
+        host: process.env.HOST
+
+    }
 }
+
 
 module.exports = config;
